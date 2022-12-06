fd7687e17fc14b5c79dddfb5be4bdb21f3c3e92f
Gustavo Prado <contato.gprado@gmail.com>
2022-12-05 21:13:07 -0300

    Update Responsive CSS and add JavaScript

---
+++ js/script.js
@@ -0,0 +1,12 @@
+if(window.SimpleSlide) {
+
+    new SimpleSlide({
+        slide: 'quote', //nome do atributo data-slide
+        time: 5000 //tempo de transição dos slides
+    });
+}
+
+if(window.SimpleAnime) {
+    new SimpleAnime();
+    
+}
